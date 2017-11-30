e5851fa8de6afd4a8f3fef37ed6c5b84cdb5ef7a
davyraes <davy-raes@telenet.be>
2017-11-30 14:27:28 +0100

    eindedag

---
+++ Telefoon/Persoon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Telefoon
+{
+    public class Persoon
+    {
+        public string Naam { get; set; }
+        public string TelefoonNr { get; set; }
+        public string Groep { get; set; }
+        public BitmapImage Foto { get; set; }
+
+        public Persoon(string nNaam,string ntelefoonNr,string nGroep,BitmapImage nFoto)
+        {
+            this.Naam = nNaam;
+            this.TelefoonNr = ntelefoonNr;
+            this.Groep = nGroep;
+            this.Foto = nFoto;
+        }
+    }
+}
